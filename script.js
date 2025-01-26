75f406580e3563457ae9c030c553855669a94cd8
Rose krieg <rosekrieg07@gmail.com>
2025-01-26 12:23:24 -0700

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Logging my favorite websites
+console.log("Favorite Websites:");
+console.log("1. https://www.etsy.com/");
+console.log("2. https://people.com/");
+console.log("3. https://www.reddit.com/");
+
+
+// Logging my favorite games
+console.log("Favorite Games:");
+console.log("1. Candyland");
+console.log("2. CK3 - AGOT");
+console.log("3. Cell to Singularity");
+
+// Logging my favorite artists and their cultural importance
+console.log("Favorite Artists:");
+console.log("1. Tamara de Lempicka");
+console.log("2. Montana's Charlie Russell");
+console.log("3. I love art that features the American West - too many to list");
+
+// Prompting the user for a question and logging their response
+const question = "What is your favorite hobby?";
+const answer = prompt(question);
+console.log("Anything True Crime, Reading, Embroidery: " + answer);
+
