c5f7e6861cc1e768949f045cc4c18d112f68d63a
Rose krieg <rosekrieg07@gmail.com>
2025-02-18 20:35:25 -0700

    f

--- HW5/script.js
+++ HW5/script.js
@@ -1,8 +1,8 @@
 // Step 1: Create the arrays
 const blankImage = "./imgs/blank.jpg"; // Path to your blank image
 const actualImages = [
-    "./imgs/alien.jpg", "./imgs/fairy.jpg", "./imgs/mouse.jpg", "./imgs/goddess.jpg",
-    "image5.jpg", "image6.jpg", "image7.jpg", "image8.jpg",
+    "./imgs/alien.jpg", "./imgs/fairy.jpg", ".", "./imgs/goddess.jpg",
+    "./imgs/bar.jpg", "./imgs/cleo.jpg", "./imgs/dogs.jpg", "./imgs/elves.jpg",
     "image9.jpg", "image10.jpg", "image11.jpg", "image12.jpg"
 ];
 
