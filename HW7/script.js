970e1c7499960340e9f9ae7cd96c5ab6d4dbade8
Rose krieg <rosekrieg07@gmail.com>
2025-02-25 20:44:49 -0700

    d

--- HW7/script.js
+++ HW7/script.js
@@ -3,11 +3,13 @@ setTimeout(() => {
     document.getElementById('scene').classList.add('fade');
     setTimeout(() => {
         document.getElementById('scene').style.display = 'none';
+        document.body.style.background = 'none';
         document.getElementById('viewer').style.display = 'block';
+        nextImage(); // Show first random image after scene transition
     }, 2000);
 }, 2500);
 
-let images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg'];
+let images = ['./Imgs/fist.png', './Imgs/handheart.jpeg', './Imgs/fist.webp', './Imgs/makeasrt.jpeg', './Imgs/redimage.jpeg','./Imgs/rose.jpeg'];
 function nextImage() {
     let randomIndex = Math.floor(Math.random() * images.length);
     document.getElementById('viewerImage').src = images[randomIndex];
