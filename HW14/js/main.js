553b09260f35a25b1b768fee5a20c6b2c450f3a5
Rose krieg <rosekrieg07@gmail.com>
2025-04-21 12:45:09 -0600

    remove extra camera line

--- HW14/js/main.js
+++ HW14/js/main.js
@@ -34,7 +34,6 @@ world.defaultContactMaterial.restitution = 0.7; // Bounciness
 // 1. Set up scene, camera, renderer
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x333333);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
