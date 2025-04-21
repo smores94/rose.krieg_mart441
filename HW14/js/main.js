6aae2ca64521f6ec7c33bc773493198abf30b1b7
Rose krieg <rosekrieg07@gmail.com>
2025-04-21 14:21:43 -0600

    imports

--- HW14/js/main.js
+++ HW14/js/main.js
@@ -1,8 +1,8 @@
 // Corrected imports (assuming local files)
-import * as THREE from './three/three.module.js';
-import { OrbitControls } from './three/controls/OrbitControls.js';
+import * as THREE from './js/three/three.module.js';
+import { OrbitControls } from './js/three/controls/OrbitControls.js';
 import * as CANNON from './js/cannon-es';
-import { Howl } from './howler.js/dist/howler.js';
+import { Howl } from './js/howler.js/dist/howler.js';
 
 // SCENE SETUP (only once!)
 const scene = new THREE.Scene();
